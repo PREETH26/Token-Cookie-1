530a63e7c70cc8d523ac02c7dc6da30e7f89d52b
PREETH26 <preethampraveencode26@gmail.com>
2025-04-08 11:25:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const encrypt = (payload) => {
+  // encrypt the payload and return token
+  x;
+};
+
+const decrypt = (token) => {
+  // return decoded payload
+};
+
+module.exports = {
+  encrypt,
+  decrypt,
+};
